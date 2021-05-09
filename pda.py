032c0917c6ce848ef80fdc0231fce059961d5c40
Erogin <31968360+Erogin@users.noreply.github.com>
2021-05-08 23:17:47 -0400

    Update pda.py

--- pda.py
+++ pda.py
@@ -18,22 +18,25 @@ class PDA:
             }
         )
     def pushdown(self, input_string):
-        state, stack, step = self.grammar['initial_state'], '', 0
-        print('step'.center(5), 'state'.center(10), 'input'.center(25), 'stack'.center(25))
-        print(str(step).center(5), state.center(10), input_string.rjust(25), 'e'.rjust(25))
-        while input_string: 
-            step+=1
-            #if state is q read input_string, if state is qx read stack
-            trans = self.grammar['rules'][(state, '' if state == 'p' else input_string[0] if state == 'q' else stack[0])]
-            #decrement input_string if q
-            input_string = input_string[1:] if state == 'q' else input_string 
-            #decrement/transform stack if qx
-            stack = trans[1] + (stack[1:] if state[1:] else stack)
-            #update state 
-            state = trans[0]
-            print(str(step).center(5), state.center(10), input_string.rjust(25) if input_string else 'e'.rjust(25), stack.rjust(25) if stack else 'e'.rjust(25)) 
-        if state in self.grammar['final_states']: print('Accepted')
-        else: print('Nope')
+        try:
+            state, stack, step = self.grammar['initial_state'], '', 0
+            print('step'.center(5), 'state'.center(10), 'input'.center(25), 'stack'.center(25))
+            print(str(step).center(5), state.center(10), input_string.rjust(25), 'e'.rjust(25))
+            while input_string: 
+                step+=1
+                #if state is q read input_string, if state is qx read stack
+                trans = self.grammar['rules'][(state, '' if state == 'p' else input_string[0] if state == 'q' else stack[0])]
+                #decrement input_string if q
+                input_string = input_string[1:] if state == 'q' else input_string 
+                #decrement/transform stack if qx
+                stack = trans[1] + (stack[1:] if state[1:] else stack)
+                #update state 
+                state = trans[0]
+                print(str(step).center(5), state.center(10), input_string.rjust(25) if input_string else 'e'.rjust(25), stack.rjust(25) if stack else 'e'.rjust(25)) 
+            if state in self.grammar['final_states']: print('Accepted')
+            else: print('Nope')
+        except:
+            print("PDA failed to consume. Not a viable language.")
 
 
 
