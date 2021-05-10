818a5c80c6cb57982432db0bdf8dcb6b20e2306f
Erogin <31968360+Erogin@users.noreply.github.com>
2021-05-10 11:18:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import pda 
 
 def main():
-    print("Input custom language?")
+    print("Input custom string?")
     run = pda.PDA()
     answer = input("Type 'Y' or 'N': ").upper()
     if answer == 'Y':
         while answer == 'Y':
             print("This program accepts a string input for a pushdown automata")
-            string = input("Language input (don't forget the '$'): ").lower()
+            string = input("String input (don't forget the '$'): ").lower()
             run.pushdown(string)
-            print('\n New language input?')
+            print('\n New string input?')
             answer = input("Type 'Y' or 'N': ").upper()
     else:
-        print("Here are some example languages:")
+        print("Here are some example strings:")
         input_strings = []
         for i in range(1, 11):
             input_strings.append('a'*i+'b'*i+'$')
